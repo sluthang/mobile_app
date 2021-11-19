6e54efcf78f1e1803dac67bca4364c36332c290a
ealberts <ealberts@student.wethinkcode.co.za>
2021-11-19 13:11:08 +0200

    Added pop up when clicking on the player button.

--- robot_worlds_app/robot_worlds_app/lib/pages/home.dart
+++ robot_worlds_app/robot_worlds_app/lib/pages/home.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,35 +22,39 @@ class _HomePageState extends State<HomePage> {
         body: buttonLayout(context));
   }
 }
+
+final GlobalKey<FormState> _formkeyPlayer = GlobalKey<FormState>();
 final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
 Future<void> showLoginDialog(BuildContext context) async {
-  return await showDialog(context: context,
-      builder: (context){
-      final TextEditingController _textController = TextEditingController();
+  return await showDialog(
+      context: context,
+      builder: (context) {
+        final TextEditingController _textController = TextEditingController();
         return AlertDialog(
           content: Form(
               key: _formkey,
               child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextFormField(
-                controller: _textController,
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return 'Please enter a correct pin';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(hintText: 'Enter Admin Pin'),
-              )
-            ],
-          )),
-          actions:<Widget> [
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: _textController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a correct pin';
+                      }
+                      return null;
+                    },
+                    decoration:
+                        const InputDecoration(hintText: 'Enter Admin Pin'),
+                  )
+                ],
+              )),
+          actions: <Widget>[
             TextButton(
               child: const Text('Login'),
-              onPressed: (){
-                if(_formkey.currentState!.validate()){
+              onPressed: () {
+                if (_formkey.currentState!.validate()) {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(content: Text('Processing Pin')),
                   );
@@ -70,12 +73,13 @@ Widget buttonLayout(BuildContext context) {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         ElevatedButton(
-          child: const Text(
-            "Player",
-            style: TextStyle(fontSize: 20.0, color: Colors.white),
-          ),
-          onPressed: () {},
-        ),
+            child: const Text(
+              "Player",
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
+            onPressed: () async {
+              await playerForm(context);
+            }),
         ElevatedButton(
           child: const Text(
             "Admin",
@@ -83,14 +87,136 @@ Widget buttonLayout(BuildContext context) {
           ),
           onPressed: () async {
             await showLoginDialog(context);
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (ctx) =>
-                const AdminScreen())
-            );
+            Navigator.push(context,
+                MaterialPageRoute(builder: (ctx) => const AdminScreen()));
           },
         ),
       ],
     ),
   );
+}
+
+final TextEditingController _ipController = TextEditingController();
+final TextEditingController _portController = TextEditingController();
+final TextEditingController _robotNameController = TextEditingController();
+final TextEditingController _robotTypeController = TextEditingController();
+
+Future<void> playerForm(BuildContext context) async {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: AlertDialog(
+            content: Stack(
+              clipBehavior: Clip.none,
+              children: <Widget>[
+                Positioned(
+                  right: -40.0,
+                  top: -40.0,
+                  child: InkResponse(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const CircleAvatar(
+                      child: Icon(Icons.close),
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                ),
+                Form(
+                  key: _formkeyPlayer,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _ipController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid IP address';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'IP ADDRESS',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _portController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid PORT';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'PORT NUMBER',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _robotNameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a name for your robot';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'ROBOT NAME',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _robotTypeController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a valid robot type';
+                            }
+                            return null;
+                          },
+                          maxLines: 1,
+                          maxLength: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'ROBOT TYPE',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          child: const Text("Connect"),
+                          onPressed: () {
+                            if (_formkeyPlayer.currentState!.validate()) {
+                              _formkeyPlayer.currentState!.save();
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      });
 }
