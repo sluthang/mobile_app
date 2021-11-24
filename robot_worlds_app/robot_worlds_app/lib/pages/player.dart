f379814ce913ad27744f378bbf81f0f8d9f71130
ealberts <ealberts@student.wethinkcode.co.za>
2021-11-24 13:53:20 +0200

    Added a look command button to the player screen and aligned it with the joystick.

--- robot_worlds_app/robot_worlds_app/lib/pages/player.dart
+++ robot_worlds_app/robot_worlds_app/lib/pages/player.dart
@@ -26,44 +26,67 @@ class _PlayerScreenState extends State<PlayerScreen> {
         title: const Text('Player Page'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Joystick(
-            size: 130,
-            isDraggable: false,
-            iconColor: Colors.blue,
-            backgroundColor: Colors.black,
-            opacity: 0.5,
-            joystickMode: JoystickModes.all,
-            onUpPressed: () {
-              // if (selectedGallery - 1 > 0)
-              //   setState(() {
-              //     selectedGallery -= 1;
-              //   });
-            },
-            onLeftPressed: () {
-              // if (selectedImage - 1 > 0)
-              //   setState(() {
-              //     selectedImage -= 1;
-              //   });
-            },
-            onRightPressed: () {
-              // if (selectedImage + 1 < galleryList[selectedGallery].length)
-              //   setState(() {
-              //     selectedImage += 1;
-              // });
-            },
-            onDownPressed: () {
-              // if (selectedGallery + 1 < galleryList.length)
-              //   setState(() {
-              //     selectedGallery += 1;
-              //   });
-            },
-            onPressed: (_direction) {
-              // print("pressed $_direction");
-              print(playerInfo.robotName);
-            }),
-      ),
+      body: buttonLayout(context),
     );
   }
+}
+
+Widget buttonLayout(BuildContext context) {
+  return Stack(children: [commandButtons(context), joystick(context)]);
+}
+
+Widget commandButtons(BuildContext context) {
+  return Container(
+      height: 420,
+      padding: const EdgeInsets.all(10.0),
+      alignment: Alignment.bottomLeft,
+      child: Row(children: [
+        ElevatedButton(
+            child: const Text(
+              "Look",
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
+            onPressed: () async {}),
+      ]));
+}
+
+Widget joystick(BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.all(10.0),
+    child: Joystick(
+        size: 130,
+        isDraggable: false,
+        iconColor: Colors.blue,
+        backgroundColor: Colors.black,
+        opacity: 0.1,
+        joystickMode: JoystickModes.all,
+        onUpPressed: () {
+          // if (selectedGallery - 1 > 0)
+          //   setState(() {
+          //     selectedGallery -= 1;
+          //   });
+        },
+        onLeftPressed: () {
+          // if (selectedImage - 1 > 0)
+          //   setState(() {
+          //     selectedImage -= 1;
+          //   });
+        },
+        onRightPressed: () {
+          // if (selectedImage + 1 < galleryList[selectedGallery].length)
+          //   setState(() {
+          //     selectedImage += 1;
+          // });
+        },
+        onDownPressed: () {
+          // if (selectedGallery + 1 < galleryList.length)
+          //   setState(() {
+          //     selectedGallery += 1;
+          //   });
+        },
+        onPressed: (_direction) {
+          // print("pressed $_direction");
+          // print(playerInfo.robotName);
+        }),
+  );
 }
